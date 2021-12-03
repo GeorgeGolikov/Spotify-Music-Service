65eed050284935607c256ee83c4702aaa88f3743
George Golikov <golikov.georgij@mail.ru>
2021-12-03 15:00:48 +0300

    Initial commit

---
+++ src/test/kotlin/ru/spbstu/trkpo/musicservice/MusicServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.spbstu.trkpo.musicservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusicServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
